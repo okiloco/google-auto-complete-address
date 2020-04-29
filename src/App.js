037fa4aa0d3ed4177beb418688f4ebdbd748dfc7
f_varga@hotmail.com <f_varga@hotmail.com>
2020-04-28 22:34:41 -0500

    Completed Component Search by Address

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
 import "./styles.css";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import { geocodeByAddress, getLatLng } from 'react-google-places-autocomplete';
 import "react-google-places-autocomplete/dist/index.min.css";
 export default function App() {
+
+  const handleAddress = ({ description }) => {
+    geocodeByAddress(description)
+      .then(results => getLatLng(results[0]))
+      .then(({ lat, lng }) =>
+        console.log('Successfully got latitude and longitude', { lat, lng })
+      )
+      .catch(error => console.error(error));
+  }
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <GooglePlacesAutocomplete
         debounce={800}
-        apiKey="AIzaSyBz5yCJO-g0mIrJs-G4ALYlNNm29IBN3RkAIzaSyBz5yCJO-g0mIrJs-G4ALYlNNm29IBN3Rk"
-        onSelect={console.log}
+        apiKey="AIzaSyBolbxxZRC2pJgG7EUPTJkgaudxRyszZ_U"
+        onSelect={handleAddress}
         renderSuggestions={(active, suggestions, onSelectSuggestion) => (
           <div className="suggestions-container">
             {suggestions.map(suggestion => (
